0c16b186ff7edabad2ad5aaa8369f819a47a545a
imahesh083 <mama00055@stud.uni-saarland.de>
2026-04-30 19:31:56 +0000

    m

--- src/main/scala/circuits/helpers/HalfAdder.scala
+++ src/main/scala/circuits/helpers/HalfAdder.scala
@@ -9,6 +9,17 @@ class HalfAdder extends Module {
   val b    = IO(Input(Bool()))
   val sum  = IO(Output(Bool()))
   val cout = IO(Output(Bool()))
-sum := a^b 
-cout := a & b
+ // 1. Summon the gates
+  val xorGate = Module(new XORGate)
+  val andGate = Module(new ANDGate)
+
+  // 2. Wire up the XOR gate for the Sum
+  xorGate.a := a
+  xorGate.b := b
+  sum       := xorGate.out
+
+  // 3. Wire up the AND gate for the Carry-out (cout)
+  andGate.a := a
+  andGate.b := b
+  cout      := andGate.out
 }
