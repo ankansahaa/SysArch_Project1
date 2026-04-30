6174e548355b1a6d1df2006fcc9809dbba7d348f
imahesh083 <mama00055@stud.uni-saarland.de>
2026-04-30 23:56:49 +0000

    actualfulladder

--- src/main/scala/circuits/helpers/FullAdder.scala
+++ src/main/scala/circuits/helpers/FullAdder.scala
@@ -13,40 +13,26 @@ class FullAdder extends Module {
 
   // We use a var to act as the "wire" that ripples the carry from bit to bit
   // In subtraction mode, the first cin is 1. In addition, it's 0.
-  var carryChain = enable_sub
 
-  for (i <- 0 until width) {
-    // 1. Instantiate the two Half Adders for this bit
-    val ha1 = Module(new HalfAdder)
-    val ha2 = Module(new HalfAdder)
-    val orGate = Module(new ORGate)
-    val xorGate = Module(new XORGate) // For the B input subtraction logic
+  val ha1    = Module(new HalfAdder)
+  val ha2    = Module(new HalfAdder)
+  val orGate = Module(new ORGate)
 
-    // 2. Logic to handle Subtraction (A + ~B + 1)
-    // If enable_sub is 1, b(i) is flipped. If 0, b(i) stays same.
-    xorGate.a := b(i)
-    xorGate.b := enable_sub
-    val b_actual = xorGate.out
+  // 2. Connect First Half Adder
+  ha1.a := a
+  ha1.b := b
 
-    // 3. Connect Half Adder 1 (adds A and modified B)
-    ha1.a := a(i)
-    ha1.b := b_actual
+  // 3. Connect Second Half Adder
+  // It takes the partial sum from HA1 and the incoming Carry-in
+  ha2.a := ha1.sum
+  ha2.b := cin
 
-    // 4. Connect Half Adder 2 (adds HA1 sum and the carry from the previous bit)
-    ha2.a := carryChain
-    ha2.b := ha1.sum
+  // 4. Output the Final Sum
+  sum := ha2.sum
 
-    // 5. Connect the bit sum to the output Vec
-    sum(i) := ha2.sum
-
-    // 6. Calculate the carry-out for this bit using an OR gate
-    orGate.a := ha1.cout
-    orGate.b := ha2.cout
-    
-    // 7. Ripple the carry to the next iteration
-    carryChain = orGate.out
-  }
-
-  // Final carry output from the last bit
-  cout := carryChain
+  // 5. Calculate Final Carry-out
+  // It's 1 if HA1 generated a carry OR HA2 generated a carry
+  orGate.a := ha1.cout
+  orGate.b := ha2.cout
+  cout     := orGate.out
 }
