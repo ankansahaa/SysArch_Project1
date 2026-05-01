d61abd3fbc87772ac0670bbcfa820095d8231c21
imahesh083 <mama00055@stud.uni-saarland.de>
2026-05-01 14:05:39 +0000

    nornnot

--- src/main/scala/circuits/helpers/nBitOR.scala
+++ src/main/scala/circuits/helpers/nBitOR.scala
@@ -8,5 +8,17 @@ class nBitOR(n: Int) extends Module {
   val a   = IO(Input(Vec(n, Bool())))
   val out = IO(Output(Bool()))
 
-  ???
+  val ors = Seq.fill(n)(Module(new ORGate))
+
+  for (i <- 0 until n) {
+    ors(i).a := a(i)
+
+    if (i == 0) {
+      ors(i).b := false.B
+    } else {
+      ors(i).b := ors(i - 1).out
+    }
+  }
+  out := ors(n - 1).out
+
 }
