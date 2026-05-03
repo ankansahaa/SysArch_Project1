4dce7f0c458c3340565950262fad0a1964b6a3e9
imahesh083 <mama00055@stud.uni-saarland.de>
2026-05-03 00:46:18 +0000

    3new

--- src/main/scala/circuits/integer/OnesComplementToSignedMagnitude.scala
+++ src/main/scala/circuits/integer/OnesComplementToSignedMagnitude.scala
@@ -8,5 +8,15 @@ class OnesComplementToSignedMagnitude(width: Int) extends Module {
   val onesComplement  = IO(Input(Vec(width, Bool())))
   val signedMagnitude = IO(Output(Vec(width, Bool())))
 
-  ???
+  val msb = onesComplement(width - 1)
+
+  for (i <- 0 until (width - 1)) {
+    val myXOR = Module(new XORGate)
+
+    myXOR.a            := onesComplement(i)
+    myXOR.b            := msb
+    signedMagnitude(i) := myXOR.out
+  }
+
+  signedMagnitude(width - 1) := msb
 }
--- src/main/scala/circuits/integer/OnesComplementToTwosComplement.scala
+++ src/main/scala/circuits/integer/OnesComplementToTwosComplement.scala
@@ -8,5 +8,23 @@ class OnesComplementToTwosComplement(width: Int) extends Module {
   val onesComplement = IO(Input(Vec(width, Bool())))
   val twosComplement = IO(Output(Vec(width, Bool())))
 
-  ???
+  val nAddSub = Module(new nBitAdderSubtractor(width))
+  val b_in    = Wire(Vec(width, Bool()))
+  val msb     = onesComplement(width - 1)
+  for (i <- 0 until width) {
+    if (i == 0) {
+      b_in(i) := msb // Add 1 only if MSB is 1 (negative)
+    } else {
+      b_in(i) := false.B // All other bits are 0
+    }
+  }
+
+  nAddSub.a          := onesComplement
+  nAddSub.b          := b_in
+  nAddSub.enable_sub := false.B
+
+  for (i <- 0 until width) {
+    twosComplement(i) := nAddSub.sum(i)
+  }
+
 }
