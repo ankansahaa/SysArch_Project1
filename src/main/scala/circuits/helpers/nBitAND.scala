432ff443c53e7e955f8bcb5c0ebd15f5705e2fe7
ankan <ansa00015@stud.uni-saarland.de>
2026-05-03 00:43:15 +0000

    nBitAnd and nBit

--- src/main/scala/circuits/helpers/nBitAND.scala
+++ src/main/scala/circuits/helpers/nBitAND.scala
@@ -8,5 +8,14 @@ class nBitAND(n: Int) extends Module {
   val a   = IO(Input(Vec(n, Bool())))
   val out = IO(Output(Bool()))
 
-  ???
+  val chain = Wire(Vec(n, Bool()))
+  chain(0) := a(0)
+
+  for (i <- 1 until n) {
+    val andGate = Module(new ANDGate)
+    andGate.a := chain(i - 1)
+    andGate.b := a(i)
+    chain(i)  := andGate.out
+  }
+  out := chain(n - 1)
 }
--- src/main/scala/circuits/helpers/nBitComparator.scala
+++ src/main/scala/circuits/helpers/nBitComparator.scala
@@ -10,5 +10,26 @@ class nBitComparator(width: Int) extends Module {
   val gt = IO(Output(Bool()))
   val eq = IO(Output(Bool()))
 
-  ???
+  val eqChain = Wire(Vec(width, Bool()))
+
+  for (i <- 0 until width) {
+    val xorGate = Module(new XORGate)
+    xorGate.a  := a(i)
+    xorGate.b  := b(i)
+    eqChain(i) := xorGate.out
+  }
+
+  val nbitor = Module(new nBitOR(width))
+  nbitor.a := eqChain
+  val notGate = Module(new NOTGate)
+  notGate.a := nbitor.out
+  eq        := notGate.out
+
+  val nbitsub = Module(new nBitAdderSubtractor(width))
+
+  nbitsub.a          := b
+  nbitsub.b          := a
+  nbitsub.enable_sub := true.B
+
+  gt := nbitsub.cout
 }
