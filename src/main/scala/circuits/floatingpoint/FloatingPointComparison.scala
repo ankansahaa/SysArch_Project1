06ca4d0259bd2adf70068b8a9c9042fad4af2635
Ankush Das <anda00007@stud.uni-saarland.de>
2026-05-13 17:02:57 +0000

    check if NaN

--- src/main/scala/circuits/floatingpoint/FloatingPointComparison.scala
+++ src/main/scala/circuits/floatingpoint/FloatingPointComparison.scala
@@ -11,5 +11,30 @@ class FloatingPointComparison extends Module {
   val b                = IO(Input(Vec(32, Bool())))
   val comparisonResult = IO(Output(Vec(2, Bool())))
 
-  ???
+  val aExpoAllOne = Module(new nBitAND(8))
+  aExpoAllOne.a := a.slice(23, 31) 
+
+  val aManistaNonZero = Module(new nBitOR(23))
+  aManistaNonZero.a := a.slice(0, 23)
+
+  val bExpoAllOne = Module(new nBitAND(8))
+  bExpoAllOne.a := b.slice(23, 31)
+
+  val bManistaNonZero = Module(new nBitOR(23))
+  bManistaNonZero.a := b.slice(0, 23)
+
+
+  val aIsNaN = Module(new ANDGate)
+  aIsNaN.a := aExpoAllOne.out
+  aIsNaN.b := aManistaNonZero.out
+  val bIsNaN = Module(new ANDGate)
+  bIsNaN.a := bExpoAllOne.out
+  bIsNaN.b := bManistaNonZero.out
+
+  val anyNaN = Module(new ORGate)
+  anyNaN.a := aIsNaN.out
+  anyNaN.b := bIsNaN.out
+
+  
+
 }
